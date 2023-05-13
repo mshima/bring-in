522d5ebacf27a8a452040a15ac91a13d030186f2
Marcelo Shima <marceloshima@gmail.com>
2023-05-13 16:06:09 -0300

    more adjusts

--- bin/sample.js
+++ bin/sample.js
@@ -1,7 +1,11 @@
 import { flyImport } from 'fly-import';
 
-const { flyImport: flyImport2 } = await flyImport('fly-import@0.1.0');
+const { flyImport: flyImport2 } = await flyImport('fly-import@0.1.3');
 
-const { flyInstall } = await flyImport2('fly-import2@npm:fly-import@0.1.0');
+console.log('fly-import@0.1.3 installed');
 
-console.log(await flyInstall('fly-import3@github:mshima/fly-import#v0.1.0'));
+const { flyInstall } = await flyImport2('fly-import2@npm:fly-import@0.1.3');
+
+console.log('fly-import2@npm:fly-import@0.1.3 installed');
+
+console.log(await flyInstall('fly-import3@github:mshima/fly-import#v0.1.3'));
